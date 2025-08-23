9dabf9d7262e2bd3a85c774a290f22e27eac2923
Fly Dev <dev@fly.io>
2025-08-23 18:37:02 +0000

    create feedback item schema

---
+++ lib/retro_board/retros/feedback_item.ex
@@ -0,0 +1,21 @@
+defmodule RetroBoard.Retros.FeedbackItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "feedback_items" do
+    field :column, :string
+    field :content, :string
+    field :author_name, :string
+
+    belongs_to :retro, RetroBoard.Retros.Retro
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(feedback_item, attrs) do
+    feedback_item
+    |> cast(attrs, [:retro_id, :column, :content, :author_name])
+    |> validate_required([:retro_id, :column, :content, :author_name])
+  end
+end
